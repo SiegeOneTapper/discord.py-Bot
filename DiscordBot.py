2d20793c157d4fa0596aeee2e635c0e09d23b506
SiegeOneTapper <48494803+SiegeOneTapper@users.noreply.github.com>
2019-08-27 12:39:35 +0100

    Testing command prefix

--- DiscordBot.py
+++ DiscordBot.py
@@ -34,7 +34,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('hello'):
         await message.channel.send('Hello!')
 
     if message.content.startswith('$ping'):
